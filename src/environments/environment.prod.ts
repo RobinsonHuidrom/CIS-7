70cec9de2317b8941adc96c269a27313d8561d95
Robinson <robinson@rkmtec.com>
2019-02-02 13:47:26 +0530

    production compilation check

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,11 +2,6 @@ export const environment = {
   production: true,
   firebaseConfig : 
   {
-    apiKey: "AIzaSyCAxc1yedeKaiMfAVDVbCDEB6jr4Y6x1Eo",
-      authDomain: "cis-demo.firebaseapp.com",
-      databaseURL: "https://cis-demo.firebaseio.com",
-      projectId: "cis-demo",
-      storageBucket: "cis-demo.appspot.com",
-      messagingSenderId: "94418661026"
+    
   }
 };
